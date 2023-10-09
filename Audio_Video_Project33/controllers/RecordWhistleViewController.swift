d4feb2763b51078fc1d8f9c48b2a040c7eacf02c
Jesse Viau <Jesse.viau92@stclairconnect.ca>
2023-10-08 21:06:13 -0400

    Setting Up

---
+++ Audio_Video_Project33/controllers/RecordWhistleViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RecordWhistleViewController.swift
+//  Audio_Video_Project33
+//
+//  Created by Jesse viau on 2023-10-08.
+//
+
+import UIKit
+
+class RecordWhistleViewController: UIViewController {
+
+    //MARK: IBOutlets
+    @IBOutlet weak var stackView: UIStackView!
+    
+    //MARK: viewDidLoad function
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Audio_Video_Project33/controllers/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Audio_Video_Project33
+//
+//  Created by Jesse viau on 2023-10-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: viewDidLoad function
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //page title
+        title = "What's that Whistle?"
+        //programmitcally add right bar button item and a back button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWhistle))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
+    }
+    
+    @objc func addWhistle() {
+        let vc = RecordWhistleViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+}
+
