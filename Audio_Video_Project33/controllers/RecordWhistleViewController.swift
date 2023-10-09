8d4908c2922bbcf0321253aed75b97669b8758b6
Jesse Viau <Jesse.viau92@stclairconnect.ca>
2023-10-08 21:51:31 -0400

    Recording from the microphone with AVAudioRecorder

--- Audio_Video_Project33/controllers/RecordWhistleViewController.swift
+++ Audio_Video_Project33/controllers/RecordWhistleViewController.swift
@@ -6,28 +6,155 @@
 //
 
 import UIKit
+import AVFoundation
 
-class RecordWhistleViewController: UIViewController {
+class RecordWhistleViewController: UIViewController, AVAudioRecorderDelegate {
 
     //MARK: IBOutlets
-    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet var stackView: UIStackView!
+    
+    //MARK: Properties
+    var recordButton: UIButton!
+    var recordingSession: AVAudioSession!
+    var whistleRecorder: AVAudioRecorder!
     
     //MARK: viewDidLoad function
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Record your whistle"
+            navigationItem.backBarButtonItem = UIBarButtonItem(title: "Record", style: .plain, target: nil, action: nil)
+
+            recordingSession = AVAudioSession.sharedInstance()
+
+            do {
+                try recordingSession.setCategory(.playAndRecord, mode: .default)
+                try recordingSession.setActive(true)
+                recordingSession.requestRecordPermission() { [unowned self] allowed in
+                    DispatchQueue.main.async {
+                        if allowed {
+                            self.loadRecordingUI()
+                        } else {
+                            self.loadFailUI()
+                        }
+                    }
+                }
+            } catch {
+                self.loadFailUI()
+            }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadRecordingUI() {
+        recordButton = UIButton()
+        recordButton.translatesAutoresizingMaskIntoConstraints = false
+        recordButton.setTitle("Tap to Record", for: .normal)
+        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+        stackView.addArrangedSubview(recordButton)
     }
-    */
 
+    func loadFailUI() {
+        let failLabel = UILabel()
+        failLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        failLabel.text = "Recording failed: please ensure the app has access to your microphone."
+        failLabel.numberOfLines = 0
+
+        stackView.addArrangedSubview(failLabel)
+    }
+    
+    override func loadView() {
+        view = UIView()
+
+            view.backgroundColor = UIColor.gray
+
+            stackView = UIStackView()
+            stackView.spacing = 30
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.distribution = UIStackView.Distribution.fillEqually
+            stackView.alignment = .center
+            stackView.axis = .vertical
+            view.addSubview(stackView)
+
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    //MARK: Class functions
+    class func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+
+    class func getWhistleURL() -> URL {
+        return getDocumentsDirectory().appendingPathComponent("whistle.m4a")
+    }
+    
+    //MARK: startRecording Method
+    func startRecording() {
+
+        view.backgroundColor = UIColor(red: 0.6, green: 0, blue: 0, alpha: 1)
+        recordButton.setTitle("Tap to Stop", for: .normal)
+        
+        let audioURL = RecordWhistleViewController.getWhistleURL()
+        
+        print(audioURL.absoluteString)
+
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            whistleRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            whistleRecorder.delegate = self
+            whistleRecorder.record()
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    //MARK: finishRecording Method
+    func finishRecording(success: Bool) {
+        view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+
+        whistleRecorder.stop()
+        whistleRecorder = nil
+
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTapped))
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+
+            let ac = UIAlertController(title: "Record failed", message: "There was a problem recording your whistle; please try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    //MARK: audioRecorderDidFinishRecording Method
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+
+    //MARK: @objc functions
+    @objc func nextTapped() {
+
+    }
+    
+    @objc func recordTapped() {
+        if whistleRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    
 }
